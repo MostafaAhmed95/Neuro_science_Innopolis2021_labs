20632dc300753ae0a4e38178d4633d3195b1bef9
MostafaAhmed95 <mostafa95.sayed@gmail.com>
2021-01-27 12:40:16 +0300

    first task working with no warning

---
+++ try_cv.py
@@ -0,0 +1,54 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+#contstants
+i_stim = 75
+c = 2
+gleak = 2
+gfast = 20
+gslow = 20
+ena = 50
+ek = -100
+eleak = -70
+phi_w = 0.15
+beta_m = -10
+gamma_m = 18
+beta_w = -10
+gamma_w = 13
+
+
+#euations
+def m_inf(v):
+    minf = 0.5 * (1+np.tanh((v-beta_m)/gamma_m))
+    return minf
+
+def w_inf(v):
+    winf = 0.5 * (1+np.tanh((v-beta_w)/gamma_w))
+    return winf
+
+def taw_eq(v):
+    #print(v)
+    taw = 1/np.cosh((v - beta_w)/ (2 * gamma_w))
+    return taw
+
+def dvt_dwt(x,t):
+    v,w = x[0],x[1]
+    v = x
+    dv_dt = (i_stim - gfast * m_inf(v)*(v-ena) - gslow * w * (v - ek) - gleak * (v - eleak))/c
+    dw_dt = phi_w * ((w_inf(v)-w)/taw_eq(v))
+    dtv = dv_dt,dw_dt
+    return dv_dt
+
+#initial values of v and w
+v_0 = -52.14
+w = 0.02
+x = [v_0,w]
+t = np.linspace(0,12,20)
+vs_ws = odeint(dvt_dwt, x, t)
+
+
+plt.plot(t,vs_ws[:,0])
+plt.xlabel('time')
+plt.ylabel('v(t)')
+plt.show()
